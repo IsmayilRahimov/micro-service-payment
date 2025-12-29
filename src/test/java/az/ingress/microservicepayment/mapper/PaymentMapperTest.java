c7433822f8384796d4b1d1d2080b8f1fe94c86f5
IsmayilRahimov <rehimovismayil650@gmail.com>
2025-12-29 19:40:49 +0400

    unit test

--- src/test/java/az/ingress/microservicepayment/mapper/PaymentMapperTest.java
+++ src/test/java/az/ingress/microservicepayment/mapper/PaymentMapperTest.java
@@ -3,6 +3,7 @@ package az.ingress.microservicepayment.mapper;
 import az.ingress.microservicepayment.entity.Payment;
 import az.ingress.microservicepayment.model.request.PaymentRequest;
 ;
+import az.ingress.microservicepayment.model.response.PaymentResponse;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -31,6 +32,26 @@ class PaymentMapperTest {
 
 
         Assertions.assertThat(actual).isEqualTo(expected);
+
+    }
+
+
+    @Test
+    void toResponseTest() {
+        var request = new Payment();
+        request.setDescription("description");
+        request.setAmount(BigDecimal.TEN);
+
+
+        var expected = new PaymentResponse();
+        expected.setDescription("description");
+        expected.setAmount(BigDecimal.TEN);
+
+        var actual = PaymentMapper.mapEntityToResponse(request);
+
+
+        Assertions.assertThat(actual).isEqualTo(expected);
+
 
     }
 
